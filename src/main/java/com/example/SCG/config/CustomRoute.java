88762e477c9ab24dbce17824ada7c55e22368334
Jisu-Shin <sjszzang3053@gmail.com>
2025-04-10 21:26:18 +0900

    Initial commit

---
+++ src/main/java/com/example/SCG/config/CustomRoute.java
@@ -0,0 +1,28 @@
+package com.example.SCG.config;
+
+import com.example.SCG.component.L1Filter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@RequiredArgsConstructor
+@Configuration
+public class CustomRoute {
+
+    private final L1Filter l1Filter;
+
+    @Bean
+    public RouteLocator cRoute(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("jisutudy", r -> r.path("/sms/**")
+                        .filters(f->f.filter(l1Filter.apply(new L1Filter.Config(true,true)))
+                        )
+                        .uri("http://localhost:8081"))
+                .route("ms2", r -> r.path("/ms2/**")
+                        .uri("http://localhost:8082"))
+                .build();
+    }
+
+}
